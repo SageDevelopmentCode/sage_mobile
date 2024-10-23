a6f460e8e9770909c2a07ea5f24e4592cd5d178e
Julius <juliuscecilia33@gmail.com>
2024-10-22 22:47:42 -0700

    frontend setup

---
+++ app/(authed)/(tabs)/(profile)/index.tsx
@@ -0,0 +1,9 @@
+import { Stack } from "expo-router";
+
+export default function ProfileLayout() {
+  return (
+    <Stack screenOptions={{ headerBackTitle: "Profile" }}>
+      <Stack.Screen name="index" />
+    </Stack>
+  );
+}
