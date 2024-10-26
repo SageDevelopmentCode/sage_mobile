ade47b194b01abb5f0e7a638ede0c73081facfdb
Julius <juliuscecilia33@gmail.com>
2024-10-25 22:59:22 -0700

    retrieve characters

--- app/(authed)/(tabs)/(profile)/index.tsx
+++ app/(authed)/(tabs)/(profile)/index.tsx
@@ -1,7 +1,10 @@
+import { HStack } from "@/components/HStack";
+import { Text } from "@/components/Text";
+import { VStack } from "@/components/VStack";
 import { characterService } from "@/services/character";
 import { Character } from "@/types/character";
 import { useCallback, useEffect, useState } from "react";
-import { Alert } from "react-native";
+import { Alert, FlatList, Touchable, TouchableOpacity } from "react-native";
 
 export default function ProfileScreen() {
   const [isLoading, setIsLoading] = useState(false);
@@ -24,5 +27,38 @@ export default function ProfileScreen() {
     fetchCharacters();
   }, [fetchCharacters]);
 
-  return <></>;
+  return (
+    <VStack flex={1} p={20} pb={0} gap={20}>
+      <HStack alignItems="center" justifyContent="center">
+        <Text fontSize={18} bold>
+          {characters.length} Characters
+        </Text>
+      </HStack>
+      <FlatList
+        data={characters}
+        keyExtractor={({ id }) => id.toString()}
+        onRefresh={fetchCharacters}
+        refreshing={isLoading}
+        ItemSeparatorComponent={() => <VStack h={20} />}
+        renderItem={({ item: character }) => (
+          <VStack
+            gap={20}
+            p={20}
+            style={{ backgroundColor: "white", borderRadius: 20 }}
+            key={character.id}
+          >
+            <TouchableOpacity>
+              <HStack alignItems="center" justifyContent="center">
+                <HStack alignItems="center">
+                  <Text fontSize={18} bold>
+                    {character.name}
+                  </Text>
+                </HStack>
+              </HStack>
+            </TouchableOpacity>
+          </VStack>
+        )}
+      />
+    </VStack>
+  );
 }
